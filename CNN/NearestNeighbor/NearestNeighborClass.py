5f51c275e282a9aace987f2f36337f5748e7bc3d
MD_Levitan <starmaksim97@gmail.com>
2017-02-05 02:48:52 +0100

    feature NNC.py cross_validation
    fix NNC.py predict_method add decorator
    feature NNC.py choose_mean
    style all
      Изменения, которые будут включены в коммит:
    	изменено:      CNN/NearestNeighbor/NearestNeighborClass.py
    	изменено:      CNN/script_mnist.py

--- CNN/NearestNeighbor/NearestNeighborClass.py
+++ CNN/NearestNeighbor/NearestNeighborClass.py
@@ -7,34 +7,165 @@ Adopted from CS231n
 
 import numpy as np
 import progressbar
+import sys
+import math
 
-class NearestNeighborClass(object):
-    def __init__(self):
-        pass
 
-    def train(self, X, y):
-        """ X is N x D where each row is an example. Y is 1-dimension of size N """
-        # the nearest neighbor classifier simply remembers all the training data
-        self.Xtr = X
-        self.ytr = y
+def choose_mean(labels):
+    """
+    Calculates the average value among labels, if average has rest part = 0.5, chooses the nearest value to first
+    element in list.
+    :param labels: list.
+    :return: average value.
+    """
+    average = sum(labels)/float(len(labels))
+    average_rest, average_int = math.modf(average)
+    if average_rest < 0.5:
+        return int(average_int)
+    if average_rest > 0.5:
+        return int(average_int+1)
+    if average_rest == 0.5:
+        if average_int < labels[0]:
+            return int(average_int+1)
+        return int(average)
 
-    def predict(self, X):
+
+def predict_decorator(distance_method):
+    def wrapper(self, X, k=1):
         """ X is N x D where each row is an example we wish to predict label for """
         num_test = X.shape[0]
-        bar = progressbar.ProgressBar(maxval=num_test, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()]).start()
+        bar = progressbar.ProgressBar(maxval=num_test, widgets=[progressbar.Bar('=', '[', ']'), ' ',
+                                                                progressbar.Percentage()]).start()
         # lets make sure that the output type matches the input type
-        Ypred = np.zeros(num_test, dtype = self.ytr.dtype)
+        Ypred = np.zeros(num_test, dtype=self.ytr.dtype)
 
         # loop over all test rows
         for i in xrange(num_test):
             # find the nearest training image to the i'th test image
             # using the L1 distance (sum of absolute value differences)
-            distances = np.sum(np.abs(self.Xtr - X[i,:]), axis = 1)
-            
-            min_index = np.argmin(distances) # get the index with smallest distance
-            Ypred[i] = self.ytr[min_index] # predict the label of the nearest example
-            bar.update(i+1)
+            distances = distance_method(self.xtr - X[i, :])
+            min_index_set = []
+
+            for counter in xrange(0, k):
+                min_index = np.argmin(distances)  # get the index with smallest distance
+                min_index_set.append(min_index)
+                distances[min_index] = sys.maxint
+
+            nearest_labels = [self.ytr[val] for val in min_index_set]
+            Ypred[i] = choose_mean(nearest_labels)  # predict the label of the nearest example
+            bar.update(i + 1)
         bar.finish()
 
         return Ypred
 
+    return wrapper
+
+
+class NearestNeighborClass(object):
+    def __init__(self):
+        self.xtr = None
+        self.ytr = None
+        pass
+
+    def train(self, x, y):
+        """ X is N x D where each row is an example. Y is 1-dimension of size N """
+        # the nearest neighbor classifier simply remembers all the training data
+        self.xtr = x[:9000]
+        self.ytr = y[:9000]
+
+    def cross_validation(self, number=3, max_value=100):
+        if self.xtr is None or self.ytr is None:
+            raise Exception("Error. Training data is missing.")
+        length_folder = len(self.xtr) / number
+        folders = [self.xtr[length_folder*i:length_folder*i+length_folder] for i in xrange(number-1)]
+        label_folders = [self.ytr[length_folder * i:length_folder * i + length_folder] for i in xrange(number-1)]
+        folders.append(self.xtr[(number-1)*length_folder:])
+        label_folders.append(self.ytr[(number-1)*length_folder:])
+        accuracy_values = []
+        for k in range(2, max_value):
+            accuracy = []
+            # for folder_counter in xrange(number):
+            for folder_counter in xrange(1):
+                nn = NearestNeighborClass()
+                X = None
+                Y = None
+                for x in xrange(number):
+                    if x != folder_counter:
+                        if X is None:
+                            X = folders[x]
+                            Y = label_folders[x]
+                        else:
+                            X = np.concatenate((X, folders[x]))
+                            Y = np.concatenate((Y, label_folders[x]))
+
+                nn.train(X, Y)
+                labels_predict = nn.predictL2(folders[folder_counter], k=k)
+                accuracy.append(np.mean(labels_predict == label_folders[folder_counter]))
+                print(accuracy)
+            accuracy_values.append(accuracy)
+        print accuracy_values
+
+    @predict_decorator
+    def predictL1(y):
+        distances = np.sum(np.abs(y), axis=1)
+        return distances
+
+    @predict_decorator
+    def predictL2(y):
+        distances = np.sqrt(np.sum(np.power(y, 2), axis=1))
+        return distances
+
+    # Usual way, but it can be faster than using decorator.
+    # def predictL1(self, X, k=1):
+    #     """ X is N x D where each row is an example we wish to predict label for """
+    #     num_test = X.shape[0]
+    #     bar = progressbar.ProgressBar(maxval=num_test,
+    #                                   widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()]).start()
+    #     # lets make sure that the output type matches the input type
+    #     Ypred = np.zeros(num_test, dtype=self.ytr.dtype)
+    #
+    #     # loop over all test rows
+    #     for i in xrange(num_test):
+    #         # find the nearest training image to the i'th test image
+    #         # using the L1 distance (sum of absolute value differences)
+    #         distances = np.sum(np.abs(self.xtr - X[i, :]), axis=1)
+    #         min_index_set = []
+    #
+    #         for counter in xrange(0, k):
+    #             min_index = np.argmin(distances)  # get the index with smallest distance
+    #             min_index_set.append(min_index)
+    #             distances[min_index] = sys.maxint
+    #
+    #         nearest_labels = [self.ytr[val] for val in min_index_set]
+    #         Ypred[i] = math.ceil(sum(nearest_labels) / k)  # predict the label of the nearest example
+    #         bar.update(i + 1)
+    #     bar.finish()
+    #
+    #     return Ypred
+    #
+    # def predictL2(self, X, k=1):
+    #     """ X is N x D where each row is an example we wish to predict label for """
+    #     num_test = X.shape[0]
+    #     bar = progressbar.ProgressBar(maxval=num_test,
+    #                                   widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()]).start()
+    #     # lets make sure that the output type matches the input type
+    #     Ypred = np.zeros(num_test, dtype=self.ytr.dtype)
+    #
+    #     # loop over all test rows
+    #     for i in xrange(num_test):
+    #         # find the nearest training image to the i'th test image
+    #         # using the L2 distance
+    #         distances = np.sqrt(np.sum(np.power(self.xtr - X[i, :], 2), axis=1))
+    #         min_index_set = []
+    #
+    #         for counter in xrange(0, k):
+    #             min_index = np.argmin(distances)  # get the index with smallest distance
+    #             min_index_set.append(min_index)
+    #             distances[min_index] = sys.maxint
+    #
+    #         nearest_labels = [self.ytr[val] for val in min_index_set]
+    #         Ypred[i] = math.ceil(sum(nearest_labels) / k)  # predict the label of the nearest example
+    #         bar.update(i + 1)
+    #     bar.finish()
+    #
+    #     return Ypred
--- CNN/script_mnist.py
+++ CNN/script_mnist.py
@@ -10,16 +10,16 @@ from NearestNeighbor import *
 from Backpropagation import *
 
 print("Hello World:)\n")
-ds=DataSet()
-util=Utilities()
+ds = DataSet()
+util = Utilities()
 
 util.test()
 
 Train, Valid, Test = ds.load_MNIST() 
-Train_images=Train[0]
-Train_labels=Train[1]
-Valid_images=Valid[0]
-Valid_labels=Valid[1]
+Train_images = Train[0]
+Train_labels = Train[1]
+Valid_images = Valid[0]
+Valid_labels = Valid[1]
 
 def float_to_hex(f):
     return hex(struct.unpack('<I', struct.pack('<f', f))[0])
@@ -32,8 +32,9 @@ def float_to_RGB(f):
     return tuple(int(value[i:i + lv / 3], 16) for i in range(0, lv, lv / 3))
 
 
-def PlotSample(ARR_im, ARR_lb,num):
-
+def PlotSample(ARR_im, ARR_lb, num=None):
+    if num is None:
+        num = len(ARR_im)
     # for i in xrange(len(ARR_im)):
     #     new_img = Image.new("RGB", (28, 28))
     #     new_img.putdata([float_to_RGB(c) for c in ARR_im[i]])
@@ -44,40 +45,57 @@ def PlotSample(ARR_im, ARR_lb,num):
     # Create folder with images
 
     figure = plt.figure()
-    lenx = len(ARR_im)
-    #lenx = 40
-    for i in range(0, lenx):
+    for i in range(0, num):
         new_img = Image.new("RGB", (28, 28))
         new_img.putdata([float_to_RGB(c) for c in ARR_im[i]])
         draw = ImageDraw.Draw(new_img)
         draw.text((0, 0), str(ARR_lb[i]))
         del draw
-        figure.add_subplot(math.floor(math.sqrt(lenx))+1, math.floor(math.sqrt(lenx))+1, i+1)
+        figure.add_subplot(math.floor(math.sqrt(num))+1, math.floor(math.sqrt(num))+1, i+1)
         plt.axis("off")
         plt.imshow(new_img)
     # new_img.save("images/out{0}.png".format(i))
     plt.tight_layout(-1, -1, -7)
-    plt.show()
-
+    plt.savefig("collage.png", fmt='png')
+    #plt.show()
     return
 
 def AnalyseData(ARR_im):
-    util.exit_with_error("COMPLETE THE FUNCTION ACCORDING TO LABSPEC!!\n")
-    
+    num = len(ARR_im)
+    maxvalue = [max([ARR_im[i][j] for i in xrange(num)]) for j in xrange(len(ARR_im[0]))]
+    minvalue = [min([ARR_im[i][j] for i in xrange(num)]) for j in xrange(len(ARR_im[0]))]
+    meanvalue = [float(sum([ARR_im[i][j] for i in xrange(num)]))/max(num, 1) for j in xrange(len(ARR_im[0]))]
+    stdvalue =  [math.sqrt(sum([(ARR_im[i][j]-meanvalue[j])**2
+                                 for i in xrange(num)]))/max(num, 1) for j in xrange(len(ARR_im[0]))]
+
+    new_img = Image.new("RGB", (28, 28))
+
+    new_img.putdata([float_to_RGB(c) for c in minvalue])
+    new_img.save("out_minvalue.png".format(i))
+
+    new_img.putdata([float_to_RGB(c) for c in maxvalue])
+    new_img.save("out_maxvalue.png".format(i))
+
+    new_img.putdata([float_to_RGB(c) for c in meanvalue])
+    new_img.save("out_meanvalue.png".format(i))
+
+    new_img.putdata([float_to_RGB(c) for c in stdvalue])
+    new_img.save("out_stdvalue.png".format(i))
     return
 
 
+nn = NearestNeighborClass()
+nn.train(Train_images, Train_labels)  # train the classifier on the training images and labels
+nn.cross_validation(number=3, max_value=30)
 
-# nn=NearestNeighborClass()
-# nn.train(Train_images, Train_labels) # train the classifier on the training images and labels
-# Labels_predict = nn.predict(Valid_images) # predict labels on the test images
-# # and now print the classification accuracy, which is the average number
-# # of examples that are correctly predicted (i.e. label matches)
-# print 'accuracy: %f' % ( np.mean(Labels_predict == Valid_labels) )
+#Labels_predict = nn.predictL2(Valid_images) # predict labels on the test images
+# and now print the classification accuracy, which is the average number
+# of examples that are correctly predicted (i.e. label matches)
+#print 'accuracy: %f' % (np.mean(Labels_predict == Valid_labels))
 
-PlotSample(Train_images, Train_labels, 2)
+#PlotSample(Train_images, Train_labels)
 
-AnalyseData(Train_images)
+#AnalyseData(Train_images)
 
 def prepare_for_backprop(batch_size, Train_images, Train_labels, Valid_images, Valid_labels):
     
